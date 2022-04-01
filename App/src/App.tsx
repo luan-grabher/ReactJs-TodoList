24e3e0bd6db18999a3aac1ab82a499c9dbd99593
Luan <lujetix@gmail.com>
2022-04-01 16:25:53 -0300

    add initial store

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,40 +1,27 @@
+//import react and component
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
+//import provider from redux
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-        <div className='text-center mt-3'>         
-          <h1>
-              [icon]
-              Lista de Tarefas
-            </h1>
-        </div>        
-        {/* add tesk input */}
-        <div className='text-center mt-3'>
-          <input type='text' className='form-control col-8' placeholder='Adicionar Tarefa' />
-          <button className='btn btn-success col-2'>Adicionar</button>
-        </div>
+import store from './store';
 
-        <hr className="col-6 mx-auto" />
-
-        {/* list of tasks */}
-        <div className='text-center mt-3 container'>
-          {/* for each task display input editable, button to change status and button to delete*/}
-          <div className="col-8">
-            <div className="row d-flex w-100 justify-content-center">
-              <div className="col-10">
-                <div className="col-8">
-                  <input type='text' className='form-control' value="Descrição tarefa 1" />                
-                </div>
-                <button className='btn btn-success col-2' title='Concluir'>Concluir</button>
-                <button className='btn btn-danger col-2'>Excluir</button>
-              </div>
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <h1>Hello World</h1>
+          {store.getState().tasks.map(task => (
+            <div key={task.id}>
+              <h2>{task.description}</h2>
+              <p>{task.completed ? 'Completed' : 'Not Completed'}</p>
             </div>
-          </div>
+          ))}
         </div>
-    </div>
-  );
+      </Provider>
+    );
+  }
 }
 
 export default App;
